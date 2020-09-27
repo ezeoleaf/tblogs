c4309f607c646b26cf229308310fd9c2f64f5b2b
Eze Olea Figueroa <ezeoleaf@gmail.com>
2020-09-27 12:05:47 -0300

    Adding tests to helpers

--- go.mod
+++ go.mod
@@ -8,5 +8,6 @@ require (
 	github.com/pkg/browser v0.0.0-20180916011732-0a3d74bf9ce4
 	github.com/rivo/tview v0.0.0-20200818120338-53d50e499bf9
 	github.com/spf13/viper v1.7.1
+	github.com/stretchr/testify v1.6.1
 	gopkg.in/yaml.v2 v2.2.4
 )
--- helpers/helpers.go
+++ helpers/helpers.go
@@ -3,6 +3,8 @@ package helpers
 import "github.com/ezeoleaf/tblogs/models"
 
 // IsIn checks if an int value is inside an int slice
+// Returns true if it's in the slice and the index of the value
+// If not found, it returns false and -1
 func IsIn(v int, s []int) (bool, int) {
 	for ix, value := range s {
 		if v == value {
